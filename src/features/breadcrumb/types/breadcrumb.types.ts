dd673e1280007c7559a9fd078e6d12277e64355b
Ahmed-El-shamy <ahmedelshamy@qutell.com>
2025-10-02 11:36:49 +0300

    feat: removing unessary changes

--- src/features/breadcrumb/types/breadcrumb.types.ts
+++ src/features/breadcrumb/types/breadcrumb.types.ts
@@ -4,20 +4,6 @@ export interface BreadcrumbItem {
     isDynamic?: boolean;
 }
 
-export interface BreadcrumbSegmentConfig {
-    label: string;
-}
-
-export interface BreadcrumbContextProps {
-    breadcrumbs: BreadcrumbItem[];
-    pushBreadcrumb: (item: BreadcrumbItem) => void;
-    popBreadcrumb: () => void;
-    clearBreadcrumbs: () => void;
-    updateBreadcrumb: (index: number, item: Partial<BreadcrumbItem>) => void;
-    generateBreadcrumbsFromPath: (pathname: string) => Promise<void>;
-    loading: boolean;
-}
-
 export interface RouteHandle {
     breadcrumb: string;
 }
