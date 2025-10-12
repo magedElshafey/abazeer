8405b6456b105c22def399404815386aeae90a03
magedElshafey <magedelshafey98@gmail.com>
2025-10-12 11:39:35 +0300

    enhancment the design for maintenance page and not found page

--- src/features/app-status/components/AnimatedTitle.tsx
+++ src/features/app-status/components/AnimatedTitle.tsx
@@ -1,12 +1,15 @@
 import { memo } from "react";
 import { useTranslation } from "react-i18next";
+
 interface AnimatedTitleProps {
   title: string;
 }
+
 const AnimatedTitle: React.FC<AnimatedTitleProps> = ({ title }) => {
   const { t } = useTranslation();
+
   return (
-    <h1 className="text-3xl md:text-5xl font-bold mb-4 text-orangeColor animate-bounce">
+    <h1 className="text-3xl md:text-5xl font-extrabold mb-4 text-orangeColor tracking-wide animate-title-pop">
       {t(title)}
     </h1>
   );
