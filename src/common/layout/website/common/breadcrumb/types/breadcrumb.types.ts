43ba7fc95bf3900fc6719befed7909da2a34ec73
Ahmed-El-shamy <ahmedelshamy@qutell.com>
2025-10-13 11:11:45 +0300

    feat: handling the integration of the breadcrumb

--- src/common/layout/website/common/breadcrumb/types/breadcrumb.types.ts
+++ src/common/layout/website/common/breadcrumb/types/breadcrumb.types.ts
@@ -2,8 +2,11 @@ export interface BreadcrumbItem {
     label: string;
     path: string;
     isDynamic?: boolean;
+    queryKey?: string[];
+    parameter?: string
 }
 
 export interface RouteHandle {
     breadcrumb: string;
+    queryKey?: string[];
 }
