3f5b21cae8c533765e5c68478b4034bd664b10b4
magedElshafey <magedelshafey98@gmail.com>
2025-11-11 15:18:29 +0200

    up

--- src/common/components/app-status/AppStatusFooter.tsx
+++ src/common/components/app-status/AppStatusFooter.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from "react-i18next";
 
 const AppStatusFooter = () => {
-  const { t } = useTranslation();
+  const { t } = useTranslation("Footer");
   return (
     <footer className="relative z-10 mt-10 text-sm text-gray-500">
       {t("© ")} {new Date().getFullYear()} {t("abazeer")}
--- src/features/static-pages/components/about/AboutCard.tsx
+++ src/features/static-pages/components/about/AboutCard.tsx
@@ -1,4 +1,4 @@
-import { memo, useMemo } from "react";
+import { memo } from "react";
 import type { AboutType } from "../../types/About.type";
 import HtmlConverter from "@/common/components/htmlConverter/HtmlConverter";
 
@@ -9,36 +9,32 @@ interface AboutCardProps {
 
 const AboutCard: React.FC<AboutCardProps> = ({ data, index }) => {
   const { name, image, description } = data;
-
-  const isReversed = useMemo(() => index % 2 !== 0, [index]);
+  console.log("description about", description);
 
   return (
     <article
-      className={`flex flex-col lg:flex-row items-center gap-6 ${
-        isReversed ? "lg:flex-row-reverse" : ""
-      }`}
+      className="py-4 px-8 rounded-lg border shadow-xl space-y-8"
       aria-labelledby={`about-title-${index}`}
       aria-describedby={`about-desc-${index}`}
     >
-      <figure className="w-full lg:w-1/2">
-        <img
-          src={image || "/images/card-big-image.png"}
-          alt={name || "About image"}
-          loading="lazy"
-          decoding="async"
-          className="w-full h-60 object-cover rounded-xl shadow-sm"
-        />
-      </figure>
-
       {/* Text Content */}
-      <section className="w-full lg:w-1/2 space-y-4 text-center  flex-col items-center">
-        <h2
-          id={`about-title-${index}`}
-          className="text-lg lg:text-xl font-semibold text-gray-900"
+      <section className="w-full  space-y-4 ">
+        <div className="flex gap-3">
+          <div className="flex-center w-12 h-12 rounded-sm bg-white shadow-sm border p-2">
+            <img
+              src={image || "/images/card-big-image.png"}
+              alt={name || "About image"}
+              loading="lazy"
+              decoding="async"
+              className="max-w-full max-h-full object-contain rounded-xl "
+            />
+          </div>
+          <h2 className="text-xl lg:text-2xl 2xl:text-3xl">{name}</h2>
+        </div>
+        <p
+          id={`about-desc-${index}`}
+          className="!text-gray-600 leading-relaxed w-full lg:w-[90%]"
         >
-          {name}
-        </h2>
-        <p id={`about-desc-${index}`} className="text-gray-600 leading-relaxed">
           <HtmlConverter html={description} />
         </p>
       </section>
