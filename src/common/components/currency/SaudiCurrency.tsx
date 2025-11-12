52f25babcb3b2a3c6ebc0b23dad506eb95531982
magedElshafey <magedelshafey98@gmail.com>
2025-11-12 09:28:53 +0200

    updates

--- src/common/components/currency/SaudiCurrency.tsx
+++ src/common/components/currency/SaudiCurrency.tsx
@@ -1,10 +1,19 @@
+import { useTranslation } from "react-i18next";
+
 const SaudiCurrency = () => {
+  const { i18n } = useTranslation();
   return (
-    <img
-      className="w-3 h-3 object-contain"
-      alt="saudi-currency"
-      src="/images/r.png"
-    />
+    <>
+      {i18n.language === "ar" ? (
+        <img
+          className="w-3 h-3 object-contain"
+          alt="saudi-currency"
+          src="/images/r.png"
+        />
+      ) : (
+        <p>SAR</p>
+      )}
+    </>
   );
 };
 
