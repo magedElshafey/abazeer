87d36f7746c0f61c7b8b99f2084ba14605f6d134
magedElshafey <magedelshafey98@gmail.com>
2025-11-30 13:50:14 +0200

    up

--- src/common/layout/website/larg-screens/header/components/LeftSide.tsx
+++ src/common/layout/website/larg-screens/header/components/LeftSide.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import Border from "../../../../../components/border/Border";
 import LoginPrompt from "@/common/components/login-prompt/LoginPrompt";
+import i18n from "@/lib/i18n/i18n";
 const LeftSide = () => {
   const { t } = useTranslation();
   return (
@@ -20,13 +21,15 @@ const LeftSide = () => {
         </Link>
       </LoginPrompt>
       <Border />
-      <Link
-        to="/order-tracking"
+      <a
+        href={`https://app.tryoto.com/shipment-tracking?lang=${i18n.language}`}
+        target="_blank"
+        rel="noopener noreferrer"
         className="text-transition"
         aria-label={t("order tracking")}
       >
         {t("order tracking")}
-      </Link>
+      </a>
     </div>
   );
 };
